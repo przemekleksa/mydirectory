ab7dc5626e79ab2ff8744a03686575282c066114
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2019-11-16 18:07:27 +0100

    setting up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h1>{{name}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  name = 'Przemek';
 }
